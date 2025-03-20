5ea7678a3043fd351978db59b058e113461d1737
sindhukavitha <sindhuk.22cse@kongu.edu>
2025-03-20 12:14:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u sindhukavikumar -p dckr_pat_uHRhPlcd2je_5EsSootHCN9G-PU
+    docker tag test1 sindhukavikumar/task2
+    docker push sindhukavikumar/task2
     
