91c0bc13aa851609610261f6755e1dc5e02818f3
sindhukavitha <sindhuk.22cse@kongu.edu>
2025-03-20 14:06:47 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@
     docker login -u sindhukavikumar -p dckr_pat_uHRhPlcd2je_5EsSootHCN9G-PU
     docker tag test1 sindhukavikumar/task2
     docker push sindhukavikumar/task2
+    docker-compose up -d
     
